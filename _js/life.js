284f3c8273e2ba81a4fed7c71e24981184d6efb6
mischizzle <michelle@picot.ca>
2014-12-03 20:39:30 +0000

    Added the first methods- aliveNeighbors (and next). Need to verify bordering neighbors

--- _js/life.js
+++ _js/life.js
@@ -1,20 +1,51 @@
+'use strict';
+
+// Conway game of life rules:
+// Any live cell with fewer than two live neighbors dies, as if caused by under-population.
+// Any live cell with two or three live neighbors lives on to the next generation.
+// Any live cell with more than three live neighbors dies, as if by overcrowding.
+// Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+
+var Life;
+
 (function(){
 
   Life = function(seed) {
     this.seed = seed;
-    this.height = seed.height;
-    this.width = seed[0].lendth;
+    this.height = seed.length;
+    this.width = seed[0].length;
     this.prevBoard = [];
     this.board = cloneArray(seed);
-
   };
 
   Life.prototype =  {
     next: function() {
+      var neighbors, y, x;
       this.prevBoard = cloneArray(this.board);
+
+      for(var y = 0; y < this.height; y++) {
+        for(var x = 0; x < this.width; x++) {
+          var neighbors = this.aliveNeighbors(this.prevBoard, x, y);
+        }
+      }
     },
 
-    tostring: function() {
+    aliveNeighbors: function(array, x, y) {
+      var sum;
+
+      var neighbors = [
+        array[y-1][x-1], array[y-1][x], array[y-1][x+1],
+        array[y][x-1], array[y][x+1],
+        array[y+1][x-1], array[y+1][x], array[y+1][x+1]
+      ].forEach( function(a) {
+        sum += +!!a;
+        console.log(sum);
+      });
+
+      return sum;
+    },
+
+    toString: function() {
       return this.board.map(function(row) {
         return row.join(' ');
       }).join('\n');
@@ -36,6 +67,12 @@ var game = new Life([
   [0, 0, 1, 0, 0],
   [0, 0, 0, 0, 0]
 ]);
-console.log(game);
+
+console.log(game + '');
+
+game.next();
+
+console.log(game + '');
+
 
 
--- _js/scribbles.js
+++ _js/scribbles.js
@@ -1,19 +1,22 @@
-(function() {
+'use strict';
 
-  var arr1 = [1, 2, 3, 4];
-  var arr2 = arr1.slice();
-  var arr3 = [[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4]];
+// (function() {/
 
-  //this.board = seed.slice().map( function(row) { return row.slice(); });
+//   var arr1 = [1, 2, 3, 4];
+//   var arr2 = arr1.slice();
+//   var arr3 = [[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4]];
 
-  function cloneArray(array) {
-    return array.slice().map( function(row) {
-      return row.slice();
-    })
-  }
-  //console.log(arr1);
-  //console.log(arr2);
-  //console.log(arr3.map(function(row) { console.log(row) }));
-  console.log(cloneArray(arr3));
+//   //this.board = seed.slice().map( function(row) { return row.slice(); });
 
-})();
+//   function cloneArray(array) {
+//     return array.slice().map( function(row) {
+//       return row.slice();
+//     })
+//   }
+//   //console.log(arr1);
+//   //console.log(arr2);
+//   //console.log(arr3.map(function(row) { console.log(row) }));
+//   console.log(cloneArray(arr3));
+
+// })();
+
